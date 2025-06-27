3d98926c125acc378f9a55b347e3307284f3cdbb
Rishik <rishik3555@gmail.com>
2025-06-27 14:55:02 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5175,
+    host: "0.0.0.0",
+    historyApiFallback: true,
+  },
   optimizeDeps: {
-    include: ['axios'],
+    include: ["axios"],
   },
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+  },
 });
